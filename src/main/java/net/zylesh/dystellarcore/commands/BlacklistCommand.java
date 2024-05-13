bee66e642d85d97eb8293f367f2b09cc5b3bc695
inti <intivioli@gmail.com>
2024-05-13 15:18:54 +0200

    Not sending target player error fix.

--- src/main/java/net/zylesh/dystellarcore/commands/BlacklistCommand.java
+++ src/main/java/net/zylesh/dystellarcore/commands/BlacklistCommand.java
@@ -3,8 +3,7 @@ package net.zylesh.dystellarcore.commands;
 import net.zylesh.dystellarcore.DystellarCore;
 import net.zylesh.dystellarcore.core.User;
 import net.zylesh.dystellarcore.core.punishments.Blacklist;
-import net.zylesh.dystellarcore.serialization.Mapping;
-import net.zylesh.dystellarcore.serialization.MariaDB;
+import net.zylesh.dystellarcore.serialization.Punishments;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -39,24 +38,19 @@ public class BlacklistCommand implements CommandExecutor {
             Blacklist blacklist = new Blacklist(reason.toString());
             userInt.punish(blacklist);
         } else {
-            DystellarCore.getAsyncManager().execute(() -> {
-                Mapping mapping = MariaDB.loadMapping(strings[1]);
-                if (mapping == null) {
-                    commandSender.sendMessage(ChatColor.RED + "This player does not exist in the database.");
-                } else {
-                    User user = MariaDB.loadPlayerFromDatabase(mapping.getUUID(), mapping.getIP(), mapping.getName());
-                    StringBuilder reason = new StringBuilder();
-                    for (int i = 2; i < strings.length; i++) {
-                        if (i == 2) reason.append(strings[i]);
-                        else reason.append(" ").append(strings[i]);
-                    }
-                    Blacklist ban = new Blacklist(reason.toString());
-                    user.addPunishment(ban);
-                    MariaDB.savePlayerToDatabase(user);
-                }
-            });
+            if (!(commandSender instanceof Player)) {
+                commandSender.sendMessage(ChatColor.RED + "You must be a player in order to punish offline players.");
+                return true;
+            }
+            Player p = (Player) commandSender;
+            StringBuilder reason = new StringBuilder();
+            for (int i = 2; i < strings.length; i++) {
+                if (i == 2) reason.append(strings[i]);
+                else reason.append(" ").append(strings[i]);
+            }
+            Blacklist ban = new Blacklist(reason.toString());
+            DystellarCore.getInstance().sendPluginMessage(p, DystellarCore.PUNISHMENT_ADD_PROXYBOUND, strings[0], Punishments.serialize(ban));
         }
-        // TODO check
         return true;
     }
 }
